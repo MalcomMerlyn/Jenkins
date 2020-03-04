3cbf40acf60863ecfc76ad34f85d2d9e2a41c36c
Mihai Jugariu <mihai.jugariu@telenav.com>
2020-03-04 14:47:50 +0200

    Hello world

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld {
+    // Class to test jenkins
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        System.out.println("Jenkins");
+    }
+}
