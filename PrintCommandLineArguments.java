b2d2c29b25a9826b7f5df34acb9d4ff3276287a4
Mihai Jugariu <mihai.jugariu@telenav.com>
2020-03-05 13:02:18 +0200

    New class that prints command line arguments

---
+++ PrintCommandLineArguments.java
@@ -0,0 +1,9 @@
+public class PrintCommandLineArguments {
+    // Main class that prints command line arguments
+    public static void main(String[] args) {
+        System.out.println("Command-line arguments:");
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
